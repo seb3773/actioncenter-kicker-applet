b06236d766ee25956817eb277d25d0a63700e099
seb3773 <99963207+seb3773@users.noreply.github.com>
2024-01-15 11:11:57 +0100

    Delete assets/action2.sh

--- assets/action2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-radio="$(LC_MESSAGES=C nmcli radio all | awk 'FNR == 2 {print $2}')"
-if [ "$1" == "check" ];then
-if [ "$radio" == "enabled" ];then echo 1;else echo 0;fi; exit
-else if [ "$radio" == "enabled" ];then nmcli radio all off;else nmcli radio all on;fi;fi
